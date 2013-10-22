2f8c920a8cb6e3ea733738bf1700c442ca604c5d
buckie <wjmartino@gmail.com>
2013-10-21 23:51:22 -0400

    added left center right click + ability to move cursor
    Signed-off-by: buckie <wjmartino@gmail.com>

--- mouse/mouse.ino
+++ mouse/mouse.ino
@@ -1,13 +1,29 @@
 
-#define TB_DIR_RIGHT 3
-#define TB_DIR_LEFT 5
-#define TB_DIR_DOWN 8
-#define TB_DIR_UP 7
 
-#define TB_CMD 11
+// BlackBerry Track Ball 
 
-int led = 13;
+//  Pin Assignments
+#define TB_DIR_RIGHT 45
+#define TB_DIR_LEFT 44
+#define TB_DIR_DOWN 43
+#define TB_DIR_UP 42
+
+// Click assignment
+#define TB_CMD 20
+
+// LED's OUT Pins
+#define TB_LED_WHITE 14
+#define TB_LED_GREEN 13
+#define TB_LED_RED 12
+#define TB_LED_BLUE 11
+
+//Mouse buttons
+#define MSB_LEFT 21
+#define MSB_CENTER 22
+#define MSB_RIGHT 23
+
 //int bt1 = 9;
+
 bool active=true;
 
 /* TrackBall */
@@ -28,10 +44,10 @@ void initTB()
 	pinMode(TB_DIR_RIGHT, INPUT);
         pinMode(TB_CMD, INPUT);
 	
-	//pinMode(TB_LED_BLUE, OUTPUT);
-	//pinMode(TB_LED_RED, OUTPUT);
-	//pinMode(TB_LED_GREEN, OUTPUT);
-	//pinMode(TB_LED_WHITE, OUTPUT);
+	pinMode(TB_LED_BLUE, OUTPUT);
+	pinMode(TB_LED_RED, OUTPUT);
+	pinMode(TB_LED_GREEN, OUTPUT);
+	pinMode(TB_LED_WHITE, OUTPUT);
 	
 	tbState.up = digitalRead(TB_DIR_UP);
 	tbState.down = digitalRead(TB_DIR_DOWN);
@@ -52,14 +68,14 @@ void pollTB(bool sendEvents=true)
 	newState.left = digitalRead(TB_DIR_LEFT);
 	newState.right = digitalRead(TB_DIR_RIGHT);
         
-	
 	//struct event tb_event;
 	//tb_event.type = TRACKBALL_MOVED;
 	if (newState.up != tbState.up) {
                 newState.c_up++;
 		if (newState.c_up > threshold)
 		{        
-                        Serial.print("UP ");
+                        Serial.print("Mouse UP ");
+                        Mouse.move(0,10);
                         reset = true;
 		}
 	}
@@ -67,7 +83,8 @@ void pollTB(bool sendEvents=true)
 		newState.c_down++;
 		if (newState.c_down > threshold)
 		{
-			Serial.print("DOWN ");
+			Serial.print("Mouse DOWN ");
+                        Mouse.move(0,-10);
                         reset = true;
 		}
 	}
@@ -75,7 +92,8 @@ void pollTB(bool sendEvents=true)
 		newState.c_left++;
 		if (newState.c_left > threshold)
 		{
-			Serial.print("LEFT ");
+			Serial.print("Mouse LEFT ");
+                        Mouse.move(-10,0);
                         reset = true;
 		}
 	}
@@ -83,7 +101,8 @@ void pollTB(bool sendEvents=true)
 		newState.c_right++;
 		if (newState.c_right > threshold)
 		{
-  			Serial.print("RIGHT ");
+  			Serial.print("Mouse RIGHT ");
+                        Mouse.move(10,0);
                         reset = true;
 		}
 	}
@@ -100,13 +119,80 @@ void pollTB(bool sendEvents=true)
 	tbState = newState;
 }
 
+/* Mouse Buttons */
+struct MSBState
+{
+	/* current state */
+	int left, right, center;
+	
+	/* cumulated state */
+	int c_left, c_right, c_center;
+} msbState;
+
+void initMSB()
+{
+	pinMode(MSB_LEFT, INPUT);
+	pinMode(MSB_CENTER, INPUT);
+	pinMode(MSB_RIGHT, INPUT);
+	
+	msbState.left = digitalRead(MSB_LEFT);
+	msbState.right = digitalRead(MSB_RIGHT);
+	msbState.center = digitalRead(MSB_CENTER);
+
+}
+
+void pollMSB(bool sendEvents=true)
+{
+	struct MSBState newState = msbState;
+	const int threshold = 1;//3
+	bool reset = false;
+	
+	newState.left = digitalRead(MSB_LEFT);
+	newState.right = digitalRead(MSB_RIGHT);
+	newState.center = digitalRead(MSB_CENTER);
+
+	if (newState.left != msbState.left) {
+		newState.c_left++;
+		if (newState.c_left > threshold)
+		{
+			Serial.print("LEFT CLICK");
+                        reset = true;
+		}
+	}
+	if (newState.right != msbState.right) {
+		newState.c_right++;
+		if (newState.c_right > threshold)
+		{
+  			Serial.print("RIGHT CLICK");
+                        reset = true;
+		}
+	}
+	if (newState.center != msbState.center) {
+		newState.c_center++;
+		if (newState.c_center > threshold)
+		{
+  			Serial.print("CENTER CLICK");
+                        reset = true;
+		}
+	}
+	
+	if (reset)
+	{
+		newState.c_left = 0;
+		newState.c_right = 0;
+		newState.c_center = 0;
+                Serial.print(" .\n");
+	}
+		
+	msbState = newState;
+}
+
 
 void setup() {        
   Serial.begin(9600);	  
   
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);    
   initTB(); 
+  initMSB();
   
 }
 
@@ -114,18 +200,33 @@ void setup() {
 void loop() {
   /* the track ball */
   pollTB();
+  pollMSB();
   
+//  Serial.print("Poll: (");
+//  Serial.print(digitalRead(TB_DIR_UP)); Serial.print(",");
+//  Serial.print(digitalRead(TB_DIR_DOWN)); Serial.print(",");
+//  Serial.print(digitalRead(TB_DIR_LEFT)); Serial.print(",");
+//  Serial.print(digitalRead(TB_DIR_RIGHT)); Serial.print(")\n");
+
   if(digitalRead(TB_CMD)==0)
     {
-    Serial.print("BUTTON\n");
-    if(active) active=false;
-    else active=true;
+    Serial.print("TrackBall BUTTON = ");
+    if(active) {
+      active=false;
+      Serial.print("true\n");
+    }
+    else {
+      active=true;
+      Serial.print("false\n");
+    }
     delay(300);
     }
     
-  if(active)
-    digitalWrite(led, HIGH);
-  else
-    digitalWrite(led, LOW);
-   
+  if(active) {
+    digitalWrite(TB_LED_BLUE, HIGH);
+  }
+  else {
+    digitalWrite(TB_LED_BLUE, LOW);
+  }
+
 }
